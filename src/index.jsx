b63db96f6ab4c55f270d956e924bc552e85d9b38
Tassu <git@tassu.me>
2018-02-14 15:19:30 +0200

    changed message
    Signed-off-by: Tassu <git@tassu.me>

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Hello World!</div>
+  <div>Hello banaani!</div>
 );
 
 class App extends Component {
