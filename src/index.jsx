100ab6fdfaa330711b7c2202cea0af2671e4862c
Tassu <git@tassu.me>
2018-02-14 15:41:58 +0200

    Revert "changed message" to clean up PR
    This reverts commit b63db96f6ab4c55f270d956e924bc552e85d9b38.

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Hello banaani!</div>
+  <div>Hello World!</div>
 );
 
 class App extends Component {
